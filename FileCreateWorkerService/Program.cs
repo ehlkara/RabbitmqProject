af45ff08efc75c4e12ade9637239cb458bab93b0
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-03-07 14:02:28 +0300

    added database with database first

--- FileCreateWorkerService/Program.cs
+++ FileCreateWorkerService/Program.cs
@@ -1,12 +1,11 @@
+using FileCreateWorkerService.Models;
 using FileCreateWorkerService.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RabbitMQ.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FileCreateWorkerService
 {
@@ -22,6 +21,11 @@ namespace FileCreateWorkerService
                 .ConfigureServices((hostContext, services) =>
                 {
                     IConfiguration Configuration = hostContext.Configuration;
+
+                    services.AddDbContext<AdventureWorks2019Context>(options =>
+                    {
+                        options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+                    });
 
                     services.AddSingleton<RabbitMQClientService>();
                     services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")), DispatchConsumersAsync = true });
